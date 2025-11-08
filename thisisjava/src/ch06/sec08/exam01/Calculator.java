19682c2cb945b27ec860d2f410061a4951db7304
RYU <RYU@192.168.56.1>
2025-11-08 18:07:23 +0900

    9주차 과제

--- thisisjava/src/ch06/sec08/exam01/Calculator.java
+++ thisisjava/src/ch06/sec08/exam01/Calculator.java
@@ -8,7 +8,7 @@ public class Calculator {
 	
 	//리턴값이 없는 메소드 선언
 	void powerOff() {
-		System.out.println("전원을 급니다.");
+		System.out.println("전원을 끕니다.");
 	}
 	
 	//호출 시 두 정수 값을 전달받고, 호출한 곳으로 결과값 int를 리턴하는 메소드 선언
